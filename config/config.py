0bc6088f91acd9173e4cfd9ef3ea6ac18512c7e3
Practic <tda.kub@gmail.com>
2025-01-31 16:17:59 +0700

    config

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ TOKEN = "6367002500:AAExGa0ZyEpPqyxiUeY-IUPP0qoehURA5q0"
 # https://t.me/template_new_bot    ссылка на бота
 # https://t.me/template_new_bot?start=1029045407  ссылка на бота с utm-меткой
 
-GROUP = -2072011276 #https://t.me/+ngwZO7hoSWNlMTli        # ссылка на группу для оплат
+GROUP = -1002072011276 #https://t.me/+ngwZO7hoSWNlMTli        # ссылка на группу для оплат
 ADMIN_LIST = [1029045407]                       # список id в телеграмм администраторов
 HELP_TG_URL = 'https://t.me/+1ndktCjS-Ik0NWEy'    # ссылка на акк поддержки в телеграмм
 REVIEW_TG_URL = 'https://t.me/+1ndktCjS-Ik0NWEy'   # ссылка на группу с отзывами
